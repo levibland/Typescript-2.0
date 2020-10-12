478091f4cf5ee71cea0e0e99b0ab4ae721df4cb1
levibland <49114610+levibland@users.noreply.github.com>
2020-10-13 10:48:19 +1300

    Create token.c

---
+++ src/token.c
@@ -0,0 +1,10 @@
+#include "include/token.h"
+
+token_t* init_token(char* value, int type)
+{
+    token_t* token = calloc(1, sizeof(struct TOKEN_STRUCT));
+    token->value = value;
+    token->type = type;
+
+    return token;
+}
