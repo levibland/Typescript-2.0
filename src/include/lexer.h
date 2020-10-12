37d14c63b41cdfb493e4d931b665d8a8635b5639
levibland <49114610+levibland@users.noreply.github.com>
2020-10-13 10:43:12 +1300

    Create lexer.h

---
+++ src/include/lexer.h
@@ -0,0 +1,34 @@
+#ifndef TST_LEXER_H
+#define TST_LEXER_H
+
+#include <stdio.h>
+
+#include "token.h"
+
+typedef struct LEXER_STRUCT
+{
+    char* src;
+    size_t src_size;
+    char c;
+    unsigned int i;
+} lexer_t;
+
+lexer_t* init_lexer(char* src);
+
+void lexer_advance(lexer_t* lexer);
+
+char lexer_peek(lexer_t* lexer, int offset);
+
+token_t* lexer_advance_with(lexer_t* lexer, token_t* token);
+
+token_t* lexer_advance_current(lexer_t* lexer, int type);
+
+void lexer_skip_whitespace(lexer_t* lexer);
+
+token_t* lexer_parse_id(lexer_t* lexer);
+
+token_t* lexer_parse_number(lexer_t* lexer);
+
+token_t* lexer_next_token(lexer_t* lexer);
+
+#endif  // TST_LEXER_H
